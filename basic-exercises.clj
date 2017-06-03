efa4d2e25ee170204e43568edaaec4f602f82c5e
freda nada <fredasnada@gmail.com>
2017-06-03 12:15:14 -0400

    if statement

--- basic-exercises.clj
+++ basic-exercises.clj
@@ -1,22 +1,23 @@
-; Use (type ) to return the type of:
-; true, 0, 0.0, 0N, 0.0M, 1/2, \f, "f"
-
 ; Pass a list to (type ) to return:
 ; clojure.lang.PersistentList$EmptyList
 (type ())
 ; clojure.lang.PersistentList
 (type (list 1 2 3))
 
-; Write a boolean if function that returns the string:
-; "This is true!" when true
-; "This is false!" when false
-
-; For the condition, simply use the Clojure primitives for truth and falsity
+; Write a boolean if function that returns a string:
+(if true
+  "the truth"
+  "the lie")
 
 ; Compare 1/2 and 0.5 using an if statement. Can you make it return true?
+(if (== 1/2 0.5)
+  "true"
+  "false")
+(if (> 1/2 0.5)
+  "false"
+  "true")
 
 ; Use cond or condp to map out your day:
-
 (condp < 60
    65 "go to the park"
    45 "learn closure"
