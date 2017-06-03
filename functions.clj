2864749462247af383023b7ffafeeef37eecc6b2
freda nada <fredasnada@gmail.com>
2017-06-03 14:39:46 -0400

    functions

--- functions.clj
+++ functions.clj
@@ -5,6 +5,15 @@
 (defn multiply-10 [x] (* x 10))
 
 ; Write a recursive function that sums a list of numbers without recur.
-(defn sum-list [x] (* x))
+(defn sum-list [nums]
+  (if (== 1 (count n))
+    n
+    (+ n (sum-list(rest n)))))
 
 ; Write a recursive function that sums a list of numbers using loop and recur.
+(defn sum-list [nums]
+  (loop [n numbers
+    sum 0]
+    (if (empty? n)
+    total
+    (recur (rest n) (+ total (first n))))))
